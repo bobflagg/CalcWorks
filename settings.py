152b18eb0a1f720ca429820eb740df61dd3b9dca
Bob Flagg <bob@calcworks.net>
2012-01-03 14:43:53 -0500

    Customizing yaba templates.

--- settings.py
+++ settings.py
@@ -52,7 +52,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/site_media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -125,7 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
 #    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 #    'samples',
@@ -172,7 +172,7 @@ TWITTER_USERNAME = 'birksworks'
 TWITTER_PASSWORD = "purna12"
 
 # Blog Name
-BLOG_NAME = 'CalcWorks'
+BLOG_NAME = 'The Unreasonable Effectiveness of Data'
 
 # Blog URL
 ROOT_BLOG_URL = 'http://localhost:8000/blog/'
@@ -185,5 +185,5 @@ DISQUS_API_KEY = "DISQUS_API_HOLDER"
 DISQUS_WEBSITE_SHORTNAME = "DISQUS_SHORT_HOLDER"
 
 # If you want to use contrib.comments set the following to True
-DJANGO_COMMENTS = False
+DJANGO_COMMENTS = True
 ################################################
--- urls.py
+++ urls.py
@@ -3,8 +3,8 @@ from os.path import dirname, join
 from samples.views import main, sample
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 site_media = join(dirname(__file__), 'site_media')
 urlpatterns = patterns('',
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     (r'^$', main),
     (r'^sample/([^\s]+)/$', sample),
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': site_media }),
